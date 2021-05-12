b177d2856f19995769eea4cea85333bf53a7c04a
Hans Fangohr <fangohr@soton.ac.uk>
2021-05-12 10:57:08 +0200

    fix typos

--- clone-and-install-repos.sh
+++ clone-and-install-repos.sh
@@ -8,13 +8,12 @@
 
 # install dependencies first
 
-
 git clone git@github.com:ubermag/ubermagutil.git
 git clone git@github.com:ubermag/discretisedfield.git
 git clone git@github.com:ubermag/ubermagtable.git
 git clone git@github.com:ubermag/micromagneticmodel.git 
 git clone git@github.com:ubermag/micromagneticdata.git 
-git clone git@github.com:ubermag/miromagnetictests.git
+git clone git@github.com:ubermag/micromagnetictests.git
 git clone git@github.com:ubermag/oommfc.git
 git clone git@github.com:ubermag/ubermag.git 
 
@@ -55,4 +54,4 @@ popd
 
 
 # clean
-pip uninstall ubermagutil discretisedfield ubermagtable micromagneticmodel micromagneticdata miromagnetictests oommfc ubermag.git 
+pip uninstall ubermagutil discretisedfield ubermagtable micromagneticmodel micromagneticdata micromagnetictests oommfc ubermag.git 
