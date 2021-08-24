a92dfaa1827be497d0bf9a32076bd8e93280d297
Martin Lang <m.lang@soton.ac.uk>
2021-08-24 15:53:51 +0200

    Use toml files in file generation.

--- repo-metadata/repo-metadata.py
+++ repo-metadata/repo-metadata.py
@@ -1,84 +1,97 @@
-import re
-import ast
-import json
-import string
-import datetime
-import requests
 import argparse
+import datetime
+import re
+import string
+
+import requests
+import tomli
 
 all_repos = ['discretisedfield']
 all_files = ['LICENSE', 'README.md']
 
 
 def authors_readme(authors):
-    with open(f'./authors.json', 'rt') as fin:
-        data = json.load(fin)
+    with open('./authors.toml', 'rb') as fin:
+        data = tomli.load(fin)
 
-    authors_line_list = []
-    affiliations_list = []
-    superscripts = {}
+    affiliations = {}
+    author_details = {}
+    affiliation_counter = 0
     for author in authors:
-        github, *affiliations = data['authors'][author]
-        
-        for affiliation in affiliations:
-            if affiliation not in affiliations_list:
-                affiliations_list.append(affiliation)
+        details = data['authors'][author]
 
-        superscripts[author] = [affiliations_list.index(affiliation) + 1
-                                for affiliation in affiliations]
+        for affiliation in details['affiliations']:
+            if affiliation not in affiliations:
+                affiliation_counter += 1
+                affiliations[affiliation] = affiliation_counter
 
-        authors_line_list.append(f'[{author}](https://github.com/{github[1:]})<sup>{",".join(map(str, superscripts[author]))}</sup>')
-        
-    header = ', '.join(authors_line_list)
+        author_details[author] = {
+            'superscripts': ','.join(
+                [str(affiliations[a]) for a in details['affiliations']]),
+            'github': details['github'][1:]  # no "@"
+        }
+
+    header = ', '.join([(fr'[{author}](https://github.com/{details["github"]})'
+                         fr'<sup>{details["superscripts"]}</sup>')
+                        for author, details in author_details.items()])
     header += '\n\n'
-    header += '  \n'.join([fr'<sup>{i+1}</sup>{data["affiliations"][a]}' for i, a in enumerate(affiliations_list)])
-    
+    header += '  \n'.join([(fr'<sup>{index}</sup> '
+                            fr'{data["affiliations"][affiliation]}')
+                           for affiliation, index in affiliations.items()])
     return header
 
 
 def contributors_readme(contributors):
-    with open(f'./authors.json', 'rt') as fin:
-        data = json.load(fin)
+    with open('./authors.toml', 'rb') as fin:
+        data = tomli.load(fin)
 
-    contributors_list = []
-    for contributor in contributors:
-        github, *_ = data['authors'][contributor]
-        contributors_list.append((contributor, github))
-        
-    return '\n'.join([f'- [{c}](https://github.com/{g[1:]})'
-                      for c, g in contributors_list])
+    return '\n'.join([(f'- [{contributor}](https://github.com/'
+                       f'{data["authors"][contributor]["github"][1:]})')
+                      for contributor in contributors])
 
 
 def placeholders(template):
-    return re.findall('\$(\w+)', template)
+    return re.findall(r'\$(\w+)', template)
 
 
 def generate_files(*, repository, files):
-    # Get metadata.json from repo's URL, extract data, and set defaults.
-    repository_url = f'https://raw.github.com/ubermag/{repository}/master/metadata.json'
-    data = ast.literal_eval(requests.get(repository_url,
-                                         headers={'Cache-Control': 'no-cache'}).text)
-    data.setdefault('year', datetime.datetime.now().year)
-    data.setdefault('package', repository)
-    data.setdefault('authors_readme', authors_readme(data['authors']))
-    data.setdefault('contributors_readme', contributors_readme(data['contributors']))
-    
+    req = requests.get(
+        f'https://raw.github.com/ubermag/{repository}/master/pyproject.toml',
+        headers={'Cache-Control': 'no-cache'})
+    pyproject = tomli.loads(req.text)
+
+    authors = [author['name'] for author in pyproject['project']['authors']]
+    data = {
+        'about': pyproject['tool']['ubermag']['about'],
+        'authors': ', '.join(authors),
+        'authors_readme': authors_readme(authors),
+        'contributors_readme': contributors_readme(
+            pyproject['tool']['ubermag']['contributors']),
+        'copyright_holder': pyproject['tool']['ubermag']['copyright_holder'],
+        'description': pyproject['project']['description'],
+        'doi': pyproject['tool']['ubermag']['doi'],
+        'package': repository,
+        'year': datetime.datetime.now().year,
+    }
+
     for file in files:
         # Read template file.
         with open(f'./templates/{file}', 'rt') as fin:
             template = string.Template(fin.read())
-    
+
         # Create content.
-        content = template.substitute({key: data[key]
-                                       for key in placeholders(template.template)})
-        
+        content = template.substitute(
+            {key: data[key]
+             for key in placeholders(template.template)})
+
         # Write file.
         with open(f'./{file}', 'wt') as fout:
             print(content, file=fout)
-            
-            
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Update repository\'s metadata')
+    parser = argparse.ArgumentParser(
+        description='Update repository\'s metadata')
     parser.add_argument('--repo', '-r',
                         type=str,
                         nargs='+',
@@ -90,7 +103,7 @@ if __name__ == '__main__':
                         required=True,
                         help='Metadata file.')
     args = parser.parse_args()
-    
+
     repos = all_repos if 'all' in args.repo else args.repo
     files = all_files if 'all' in args.file else args.file
 
