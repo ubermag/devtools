e0bbacff55c9193ac37e05c58cb7e5ee86326009
Martin Lang <m.lang@soton.ac.uk>
2022-01-31 16:19:57 +0100

    Delete update script.

--- update-repos.sh
+++
@@ -1,6 +0,0 @@
-for dir in ubermagutil discretisedfield ubermagtable micromagneticmodel micromagneticdata micromagnetictests oommfc ubermag; do
-    echo $dir
-    pushd repos/$dir
-    git pull -v
-    popd
-done
