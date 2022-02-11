8a31b6ab0f2bf5f559eea8bd5feb6e63e0ec1bbc
Martin Lang <m.lang@soton.ac.uk>
2022-02-11 18:56:56 +0100

    Task to update metadata locally

--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 """Tasks to set up ubermag for development."""
 import contextlib
 import os
+import shutil
 
 from invoke import task
 
@@ -76,6 +77,49 @@ def uninstall(c):
 def init_pre_commit(c):
     """Initialise pre-commit for all subpackages."""
     _execute_command(c, 'pre-commit install')
+
+
+@task(
+    help={
+        'repos': 'List of repos to update; all are updated if not specified',
+        'files': 'List of files to update; all are updated if not specified',
+        'push': 'Push changes; defaults to false',
+        'pr': ('Use a dedicated branch (repo-metadata); useful to create a PR'
+               'defaults to true')},
+    iterable=['files', 'repos']
+)
+def update_repo_metadata(c, repos, files, push=False, pr=True):
+    """Update repo-metadata locally.
+
+    The option ``pr`` uses the branch repo-metadata. If specified the function
+    switches to that branch (creates it if necessary) and aplies all changes
+    there. It does NOT switch back to the previous branch.
+    """
+    from repometadata.repometadata import generate_files
+    if len(files) == 0:
+        files = None  # automatically pick all files
+    if len(repos) == 0:
+        repos = REPOLIST
+
+    with _change_directory('repometadata'):
+        for repo in repos:
+            print(f'repo {repo} ...')
+            generate_files(
+                repository=repo,
+                files=files,
+                pyproject_path=f'../{REPODIR}/{repo}/pyproject.toml')
+
+            shutil.copytree(src=f'{repo}',
+                            dst=f'../{REPODIR}/{repo}',
+                            dirs_exist_ok=True)
+
+            with _change_directory(f'../{REPODIR}/{repo}'):
+                if pr:
+                    c.run('git checkout -b repo-metadata')
+                c.run('git commit -m "Update repo metadata."')
+                if push:
+                    print('push... [COMMENTED OUT]')
+                    c.run('git push -u origin')
 
 
 def _clone_repos(c, protocol, repolist):
